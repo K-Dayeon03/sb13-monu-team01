05f6333ea367bd42f2dc257dde8ed01395450188
chaewon021734 <zeta01873@gmail.com>
2026-08-12 14:45:55 +0900

    Initial commit

---
+++ src/test/java/com/project/monu/Sb13MonuTeam01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.monu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb13MonuTeam01ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
