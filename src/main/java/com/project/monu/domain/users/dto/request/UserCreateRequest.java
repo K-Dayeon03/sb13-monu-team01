9fbba3b3367b34965d1ff17806eb7bb0a65ca362
chaewon021734 <zeta01873@gmail.com>
2026-08-17 00:01:08 +0900

    feat: 이메일 형식 검증 구현 (TDD - GREEN) 추가  - UserCreateRequestTest.java @Email을 통한 이메일 형식 검증 추가
    TDD
     - GREEN: 이메일 형식 검증을 구현하여 테스트 통과

--- src/main/java/com/project/monu/domain/users/dto/request/UserCreateRequest.java
+++ src/main/java/com/project/monu/domain/users/dto/request/UserCreateRequest.java
@@ -1,6 +1,10 @@
 package com.project.monu.domain.users.dto.request;
 
+import jakarta.validation.constraints.Email;
+
 public record UserCreateRequest(
+
+    @Email
     String email,
     String nickname,
     String password
