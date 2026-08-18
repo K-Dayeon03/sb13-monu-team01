7ca306289fe00ad6e946b9318242e9ad26417255
seok <kseok.dev@gmail.com>
2026-08-18 09:12:43 +0900

    feat: 네이버 뉴스 API 연동 클라이언트 구현

---
+++ src/main/java/com/project/monu/domain/article/collector/naver/NaverClientConfig.java
@@ -0,0 +1,26 @@
+package com.project.monu.domain.article.collector.naver;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
+
+@Configuration
+public class NaverClientConfig {
+
+    @Value("${naver.api.client-id}")
+    private String clientId;
+
+    @Value("${naver.api.client-secret}")
+    private String clientSecret;
+
+
+    @Bean
+    public RestClient naverRestClient() {
+        return RestClient.builder()
+                .baseUrl("https://naverapihub.apigw.ntruss.com/search/v1/news")
+                .defaultHeader("X-NCP-APIGW-API-KEY-ID", clientId)
+                .defaultHeader("X-NCP-APIGW-API-KEY", clientSecret)
+                .build();
+    }
+}
