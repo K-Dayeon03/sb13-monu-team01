e5b293105f2e2f064c451c2ac51e8a9563fccfed
chaewon021734 <zeta01873@gmail.com>
2026-08-18 16:27:20 +0900

    feat: 닉네임 필수 검증 구현 (TDD - GREEN)
    수정부분
     - UserCreateRequest.java 닉네임 필수 검증을 위한 @NotBlank 추가
    
    TDD
     - GREEN: 닉네임 필수 검증을 구현하여 테스트 통과

--- src/main/java/com/project/monu/domain/users/dto/request/UserCreateRequest.java
+++ src/main/java/com/project/monu/domain/users/dto/request/UserCreateRequest.java
@@ -1,12 +1,16 @@
 package com.project.monu.domain.users.dto.request;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 
 public record UserCreateRequest(
 
     @Email
     String email,
+
+    @NotBlank
     String nickname,
+
     String password
 ) {
 }
