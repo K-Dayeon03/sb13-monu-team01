8a6721dc7c8eb0e04cf8f7c6f9dda6b72e573f16
Seungwan <seung6361@naver.com>
2026-08-19 16:07:14 +0900

    fix: MonuApplication 중복 EnableJpaAuditing 제거 (JpaAuditingConfig와 충돌)

--- src/main/java/com/project/monu/MonuApplication.java
+++ src/main/java/com/project/monu/MonuApplication.java
@@ -2,10 +2,8 @@ package com.project.monu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
 public class MonuApplication {
   public static void main(String[] args) {
     SpringApplication.run(MonuApplication.class, args);
