b906c71f6bec5052c26aa4799210e170d253f3fe
Seungwan <seung6361@naver.com>
2026-08-19 11:48:39 +0900

    feat: InterestRepository 구현

--- src/main/java/com/project/monu/domain/interest/repository/InterestRepository.java
+++ src/main/java/com/project/monu/domain/interest/repository/InterestRepository.java
@@ -1,4 +1,14 @@
 package com.project.monu.domain.interest.repository;
 
-public class InterestRepository {
+import com.project.monu.domain.interest.entity.Interest;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface InterestRepository extends JpaRepository<Interest, UUID> {
+
+    @Query("select i.name from Interest i")
+    List<String> findAllNames();
 }
