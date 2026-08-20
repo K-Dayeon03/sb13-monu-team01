05b01e0c87d9c4379a909648d1883ca840b68b2e
haebppy <ini332853@kakao.com>
2026-08-20 15:22:21 +0900

    feat: 댓글 등록 API 및 저장 로직 구현

--- src/main/java/com/project/monu/domain/comment/dto/request/CommentCreateRequest.java
+++ src/main/java/com/project/monu/domain/comment/dto/request/CommentCreateRequest.java
@@ -1,4 +1,22 @@
 package com.project.monu.domain.comment.dto.request;
 
-public record CommentCreateRequest() {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.util.UUID;
+
+public record CommentCreateRequest(
+
+        @NotNull
+        UUID articleId,
+
+        @NotNull
+        UUID userId,
+
+        @NotBlank
+        @Size(max = 500)
+        String content
+
+) {
 }
--- src/main/java/com/project/monu/domain/comment/service/BasicCommentService.java
+++ src/main/java/com/project/monu/domain/comment/service/BasicCommentService.java
@@ -1,11 +1,17 @@
 package com.project.monu.domain.comment.service;
 
+import com.project.monu.domain.article.entity.Article;
+import com.project.monu.domain.article.repository.ArticleRepository;
 import com.project.monu.domain.comment.dto.CommentDto;
 import com.project.monu.domain.comment.dto.CommentLikeDto;
 import com.project.monu.domain.comment.dto.request.CommentCreateRequest;
 import com.project.monu.domain.comment.dto.request.CommentUpdateRequest;
+import com.project.monu.domain.comment.entity.Comment;
 import com.project.monu.domain.comment.repository.CommentLikeRepository;
 import com.project.monu.domain.comment.repository.CommentRepository;
+import com.project.monu.domain.users.entity.User;
+import com.project.monu.domain.users.repository.UserRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -17,10 +23,34 @@ public class BasicCommentService implements CommentService {
 
     private final CommentRepository commentRepository;
     private final CommentLikeRepository commentLikeRepository;
+    private final ArticleRepository articleRepository;
+    private final UserRepository userRepository;
 
+    @Transactional
     @Override
     public CommentDto create(CommentCreateRequest request) {
-        return null;
+
+
+        // 공통 예외 구조 맞출 때 ArticleNotFoundException, UserNotFoundException 등으로 바꿀 것
+        Article article = articleRepository.findById(request.articleId())
+                .orElseThrow(() -> new IllegalArgumentException("기사를 찾을 수 없습니다."));
+
+        User user = userRepository.findById(request.userId())
+                .orElseThrow(() -> new IllegalArgumentException("사용자를 찾을 수 없습니다."));
+
+        Comment comment = new Comment(article, user, request.content());
+
+        Comment savedComment = commentRepository.save(comment);
+
+        return new CommentDto(
+                savedComment.getId(),
+                article.getId(),
+                user.getId(),
+                user.getNickname(),
+                savedComment.getContent(),
+                0L,
+                false,
+                savedComment.getCreatedAt());
     }
 
     @Override
